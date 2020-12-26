0a5bf57d493e2df1a96b43fc7d88c2f2dc5b14f2
KeikeiSo <calliasu2018@gmail.com>
2020-12-26 12:18:50 -0500

    Create hello.py
    an hello world in tkinter

---
+++ hello.py
@@ -0,0 +1,14 @@
+"""
+Created on Sat Dec 26 12:08:28 2020
+
+@author: callia
+"""
+import tkinter as tk
+
+root = tk.Tk()
+
+mylabel = tk.Label(root, text = "hello world!")
+
+mylabel.pack()
+
+root.mainloop()
